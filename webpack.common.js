132b06db1b5430da388a6bf178aaef301a925779
Vinson 2 <vinsonhe2@gmail.com>
2025-03-17 14:42:52 -0700

    Add content to template.html

--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,10 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
             }
         ]
     }
